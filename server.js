c3831bf2b93940062b6accc350e8cf9659ee5313
harsh-p4tel <hp17harsh31@gmail.com>
2022-08-15 01:50:05 +0530

    server only

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(Cors());
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build/index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+//     app.use(express.static("client/build"));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname + "/client/build/index.html"));
+//     });
+// }
 
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewURLParser: true })
